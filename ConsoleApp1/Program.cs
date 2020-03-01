6f51c4135bd7a42767c0ff60f8feae995313c5b0
dannyLiu <h152074716@gmail.com>
2020-03-01 23:43:33 +0800

    用HttpClient取得網頁內容

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
 
 namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HttpClient httpClient = new HttpClient();
+
+            string url = "https://dannyliu.me";
+            var responseMessage = await httpClient.GetAsync(url); //發送請求
+
+            //檢查回應的伺服器狀態StatusCode是否是200 OK
+            if (responseMessage.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                string response = responseMessage.Content.ReadAsStringAsync().Result;//取得內容
+                
+                Console.WriteLine(response);
+            }
+
+            Console.ReadKey();
         }
     }
 }
